cd375309a38b5fa8f856e52ba84aa57e25129d88
HassanKhan123 <hk2602809@gmail.com>
2023-11-26 14:20:27 +0100

    added: two way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,17 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningalertComponent } from './warningalert/warningalert.component';
 import { SuccessalertComponent } from './successalert/successalert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent, WarningalertComponent, SuccessalertComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningalertComponent,
+    SuccessalertComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
